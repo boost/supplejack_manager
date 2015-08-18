50e4d9111753cf8559c9d4d558ffaa18979ff82e
Benedict Aluan <benedict.aluan@gmail.com>
2015-08-18 14:42:43 +1200

    Updated javascript includes.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,6 +22,17 @@
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
+//= require foundation/modernizr.foundation
+//= require foundation/jquery.placeholder
+//= require foundation/jquery.foundation.topbar
+//= require foundation/jquery.foundation.reveal
+//= require foundation/jquery.foundation.buttons
+//= require foundation/jquery.foundation.tabs
+//= require foundation/jquery.foundation.forms
+//= require foundation/jquery.foundation.tooltips
+//= require foundation/app
+//= require dataTables/jquery.dataTables
+//= require datatables.foundation
 //= require jquery.ui.datepicker
 //= require jquery.ui.slider
 //= require jquery.ui.timepicker
@@ -41,7 +52,3 @@
 //= require partners
 //= require sources
 //= require suppressed_collections
-//= require_tree
-//= require foundation
-//= require datatables.foundation
-//= require dataTables/jquery.dataTables
